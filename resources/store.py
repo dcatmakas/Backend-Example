f54dfe334cfa6029f6d1b2a069c0589d213122aa
Doğukan <dcatmakas@gmail.com>
2024-02-18 15:26:15 +0300

    SQLArchemy was integrated into the project.

--- resources/store.py
+++ resources/store.py
@@ -2,7 +2,11 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from db import db
+from models import StoreModel
 from schemas import StoreSchema
 
 
@@ -12,35 +16,35 @@ blp = Blueprint("Stores", __name__, description = "Operations on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message = "Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, "Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted."}
 
 
 @blp.route("/store")
 class StoreList(MethodView):
     @blp.response(201, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
 
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
     def post(self, store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(
-                    400,
-                    message = "Store already exists."
-                )
-        store_id = uuid.uuid4().hex
-        new_store = {**store_data, "id": store_id}
-        stores[store_id] = new_store
-        return new_store, 201
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message = "A store with that name already exists."
+            )
+        except SQLAlchemyError:
+            abort(
+                500,
+                message = "An error occured creating the store."
+            )
